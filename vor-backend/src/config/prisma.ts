6f8670b412136a72bce293d5361df5dcd533dcf6
Muhajir Muslimin <muhajirmuslimin142199@gmail.com>
2026-07-01 12:06:35 +0800

    fix: use standard PrismaClient, remove adapter-pg

--- vor-backend/src/config/prisma.ts
+++ vor-backend/src/config/prisma.ts
@@ -1,10 +1,5 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
 
-const prisma = new PrismaClient({
-  adapter: new PrismaPg({
-    connectionString: process.env.DATABASE_URL,
-  }),
-})
+const prisma = new PrismaClient()
 
 export default prisma
